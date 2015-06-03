27f5ca5219fdefc4cfa873067f5847a7d691db29
Dmitri Rubinstein <dmitri.rubinstein@dfki.de>
2015-06-03 12:37:30 +0200

    Improved multicast NIO test utility.

--- src/test/java/org/fiware/kiara/ps/MulticastNIOReceiver.java
+++ src/test/java/org/fiware/kiara/ps/MulticastNIOReceiver.java
@@ -22,27 +22,36 @@ import org.fiware.kiara.util.HexDump;
 public class MulticastNIOReceiver {
 
     public static void main(String[] args) throws Exception {
-        NetworkInterface ni = NetworkInterface.getByName("wlp3s0");  // wlp3s0
+        java.util.Enumeration<java.net.NetworkInterface> nets = java.net.NetworkInterface.getNetworkInterfaces();
+        java.net.NetworkInterface ni = null;
+        for (java.net.NetworkInterface netint : java.util.Collections.list(nets)) {
+            if (netint.isUp() && netint.supportsMulticast()) {
+                ni = netint;
+            }
+        }
+
         InetAddress group = InetAddress.getByName("239.255.0.1");
 
         DatagramChannel dc = DatagramChannel.open(StandardProtocolFamily.INET)
                 .setOption(StandardSocketOptions.SO_REUSEADDR, true)
                 .bind(new InetSocketAddress(1024))
                 .setOption(StandardSocketOptions.IP_MULTICAST_IF, ni);
-        MembershipKey key = dc.join(group, ni);
 
-        System.err.printf("MulticastJoin: Address: %s, NetIf: %s, Key: %s%n", group, ni, key);
+        for (java.net.NetworkInterface netint : java.util.Collections.list(java.net.NetworkInterface.getNetworkInterfaces())) {
+            if (netint.isUp() && netint.supportsMulticast()) {
+                MembershipKey key = dc.join(group, netint);
+                System.err.printf("MulticastJoin: Address: %s, NetIf: %s, Key: %s%n", group, ni, key);
+            }
+        }
 
         ByteBuffer byteBuffer = ByteBuffer.allocate(1500);
         while (true) {
-            if (key.isValid()) {
-                byteBuffer.clear();
-                InetSocketAddress sa = (InetSocketAddress) dc.receive(byteBuffer);
-                byteBuffer.flip();
+            byteBuffer.clear();
+            InetSocketAddress sa = (InetSocketAddress) dc.receive(byteBuffer);
+            byteBuffer.flip();
 
-                System.out.println("Multicast received from " + sa.getHostString());
-                System.out.println(HexDump.dumpHexString(byteBuffer));
-            }
+            System.out.println("Multicast received from " + sa.getHostString());
+            System.out.println(HexDump.dumpHexString(byteBuffer));
         }
     }
 }
