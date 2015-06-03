825589d1fa5bf4f8d6169c9682abd1c341644e56
Dmitri Rubinstein <dmitri.rubinstein@dfki.de>
2015-06-03 11:32:35 +0200

    Added testing utilities for UDP multicast.

---
+++ src/test/java/org/fiware/kiara/ps/MulticastReceiver.java
@@ -0,0 +1,36 @@
+package org.fiware.kiara.ps;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author lycog
+ */
+public class MulticastReceiver {
+
+    public static void main(String[] args) {
+        MulticastSocket socket = null;
+        DatagramPacket inPacket = null;
+        byte[] inBuf = new byte[256];
+        try {
+            //Prepare to join multicast group
+            socket = new MulticastSocket(1024);
+            InetAddress address = InetAddress.getByName("239.255.0.1");
+            //socket.setInterface(address);
+            socket.joinGroup(address);
+
+            //SocketAddress socketAddress = new SocketAddress(groupIp, groupPort);
+            //NetworkInterface networkInterface = NetworkInterface.getByName(interfaceName);
+            //socket.joinGroup(null, null);
+            while (true) {
+                inPacket = new DatagramPacket(inBuf, inBuf.length);
+                socket.receive(inPacket);
+                String msg = new String(inBuf, 0, inPacket.getLength());
+                System.out.println("From " + inPacket.getAddress() + " Msg : " + msg);
+            }
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+    }
+}
---
+++ src/test/java/org/fiware/kiara/ps/MulticastSender.java
@@ -0,0 +1,43 @@
+package org.fiware.kiara.ps;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ * @author lycog
+ */
+public class MulticastSender {
+
+    public static void main(String[] args) {
+        DatagramSocket socket = null;
+        DatagramPacket outPacket = null;
+        byte[] outBuf;
+        final int PORT = 1024;
+
+        try {
+            socket = new DatagramSocket();
+            long counter = 0;
+            String msg;
+
+            while (true) {
+                msg = "This is multicast! " + counter;
+                counter++;
+                outBuf = msg.getBytes();
+
+                //Send to multicast IP address and port
+                InetAddress address = InetAddress.getByName("239.255.0.1");
+                outPacket = new DatagramPacket(outBuf, outBuf.length, address, PORT);
+
+                socket.send(outPacket);
+
+                System.out.println("Server sends : " + msg);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException ie) {
+                }
+            }
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+    }
+}
