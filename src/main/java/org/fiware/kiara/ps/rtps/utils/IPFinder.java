16e780175bc975108a7ea3ee2692cfd09e26e5c4
Dmitri Rubinstein <dmitri.rubinstein@dfki.de>
2015-06-03 10:58:51 +0200

    Fixed bugs.

--- src/main/java/org/fiware/kiara/ps/rtps/utils/IPFinder.java
+++ src/main/java/org/fiware/kiara/ps/rtps/utils/IPFinder.java
@@ -5,9 +5,12 @@ import java.net.DatagramSocket;
 import java.net.Inet4Address;
 import java.net.Inet6Address;
 import java.net.InetAddress;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.fiware.kiara.ps.rtps.common.Locator;
 import org.fiware.kiara.ps.rtps.common.LocatorKind;
@@ -17,15 +20,47 @@ public class IPFinder {
 	
 	public static List<InfoIP> getIPs() {
 		ArrayList<InfoIP> retVal = new ArrayList<InfoIP>();
-		
+
 		try {
+                    java.util.Enumeration<java.net.NetworkInterface> nets = java.net.NetworkInterface.getNetworkInterfaces();
+                    for (java.net.NetworkInterface netint : java.util.Collections.list(nets)) {
+                        java.util.Enumeration<java.net.InetAddress> inetAddresses = netint.getInetAddresses();
+                        while (inetAddresses.hasMoreElements()) {
+                            final InetAddress currentIP = inetAddresses.nextElement();
+                            final InfoIP infoIP = new InfoIP();
+
+                            if (currentIP.getHostAddress().indexOf(":") >= 0) { // IPv6
+                                    System.out.println("IPv6");
+                                    Inet6Address currentIPv6 = (Inet6Address) currentIP;
+                                    infoIP.type = IPTYPE.IPv6;
+                                    infoIP.name = currentIPv6.getHostAddress();
+                                    if (!parseIPv6(infoIP)) {
+                                            infoIP.type = IPTYPE.IPv6_LOCAL;
+                                    }
+                                    infoIP.scopeId = currentIPv6.getScopeId();
+                                    retVal.add(infoIP);
+                            } else if (currentIP.getHostAddress().matches("^\\d+\\.\\d+\\.\\d+\\.\\d+$")) { // IPv4
+                                    System.out.println("IPv4");
+                                    infoIP.type = IPTYPE.IPv4;
+                                    infoIP.name = currentIP.getHostAddress();
+                                    if (!parseIPv4(infoIP)) {
+                                            infoIP.type = IPTYPE.IPv4_LOCAL;
+                                    }
+                                    retVal.add(infoIP);
+                            }
+
+                            System.out.println(currentIP.getHostAddress());
+                            //retVal.add(allMyIps[i]);
+                        }
+                    }
+/*
 			InetAddress localhost = InetAddress.getLocalHost();
 			InetAddress[] allMyIps = InetAddress.getAllByName(localhost.getCanonicalHostName());
-			
-			if (allMyIps != null && allMyIps.length > 1) {
-				
+
+			if (allMyIps != null && allMyIps.length > 0) {
+
 				for (int i = 0; i < allMyIps.length; ++i) {
-					
+
 					InetAddress currentIP = allMyIps[i];
 					
 					InfoIP infoIP = new InfoIP();
@@ -53,14 +88,15 @@ public class IPFinder {
 					System.out.println(allMyIps[i].getHostAddress());
 					//retVal.add(allMyIps[i]);
 				}
-				
+
 			}
-			
-		} catch (UnknownHostException e) {
+                        */
+
+		} catch (SocketException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return retVal;
 	}
 	
@@ -78,25 +114,62 @@ public class IPFinder {
 		} 
 		
 		return locators;
-		
+
 	}
-	
+
+        public static Inet4Address getFirstIPv4Address() {
+            try {
+                java.util.Enumeration<java.net.NetworkInterface> nets = java.net.NetworkInterface.getNetworkInterfaces();
+                for (java.net.NetworkInterface netint : java.util.Collections.list(nets)) {
+                    java.util.Enumeration<java.net.InetAddress> inetAddresses = netint.getInetAddresses();
+                    while (inetAddresses.hasMoreElements()) {
+                        InetAddress inetAddress = inetAddresses.nextElement();
+                        if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                            return (Inet4Address)inetAddress;
+                        }
+                    }
+                }
+            } catch (SocketException ex) {
+                ex.printStackTrace();
+            }
+            return null;
+        }
+
+        public static Inet6Address getFirstIPv6Address() {
+            try {
+                java.util.Enumeration<java.net.NetworkInterface> nets = java.net.NetworkInterface.getNetworkInterfaces();
+                for (java.net.NetworkInterface netint : java.util.Collections.list(nets)) {
+                    java.util.Enumeration<java.net.InetAddress> inetAddresses = netint.getInetAddresses();
+                    while (inetAddresses.hasMoreElements()) {
+                        InetAddress inetAddress = inetAddresses.nextElement();
+                        if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet6Address) {
+                            return (Inet6Address)inetAddress;
+                        }
+                    }
+                }
+            } catch (SocketException ex) {
+                ex.printStackTrace();
+            }
+            return null;
+        }
+
 	public static Inet4Address getFirstIPv4Adress() {
-		try {
-			InetAddress[] allMyIps = InetAddress.getAllByName(InetAddress.getLocalHost().getCanonicalHostName());
-			for (InetAddress it : allMyIps) {
-				if (!it.isLoopbackAddress() && it instanceof Inet4Address) {
-					System.out.println(it.getHostName());
-					return (Inet4Address) it;
-				}
-			}
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
+            return getFirstIPv4Address();
+//		try {
+//			InetAddress[] allMyIps = InetAddress.getAllByName(InetAddress.getLocalHost().getCanonicalHostName());
+//			for (InetAddress it : allMyIps) {
+//				if (!it.isLoopbackAddress() && it instanceof Inet4Address) {
+//					System.out.println(it.getHostName());
+//					return (Inet4Address) it;
+//				}
+//			}
+//		} catch (UnknownHostException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return null;
 	}
-	
+
 	public static LocatorList getIPv6Adress() {
 		LocatorList locators = new LocatorList();
 		List<InfoIP> ipNames = IPFinder.getIPs();
@@ -113,23 +186,24 @@ public class IPFinder {
 		return locators;
 		
 	}
-	
+
 	public static Inet6Address getFirstIPv6Adress() {
-		try {
-			InetAddress[] allMyIps = InetAddress.getAllByName(InetAddress.getLocalHost().getCanonicalHostName());
-			for (InetAddress it : allMyIps) {
-				if (!it.isLoopbackAddress() && it instanceof Inet6Address) {
-					System.out.println(it.getHostName());
-					return (Inet6Address) it;
-				}
-			}
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return null;
+            return getFirstIPv6Address();
+//		try {
+//			InetAddress[] allMyIps = InetAddress.getAllByName(InetAddress.getLocalHost().getCanonicalHostName());
+//			for (InetAddress it : allMyIps) {
+//				if (!it.isLoopbackAddress() && it instanceof Inet6Address) {
+//					System.out.println(it.getHostName());
+//					return (Inet6Address) it;
+//				}
+//			}
+//		} catch (UnknownHostException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return null;
 	}
-	
+
 	public static LocatorList getAllIPAdress() {
 		LocatorList locators = new LocatorList();
 		List<InfoIP> ipNames = IPFinder.getIPs();
--- src/test/java/org/fiware/kiara/ps/NewMain.java
+++ src/test/java/org/fiware/kiara/ps/NewMain.java
@@ -1,6 +1,9 @@
 package org.fiware.kiara.ps;
 
 
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.fiware.kiara.ps.rtps.common.Locator;
 import org.fiware.kiara.ps.rtps.messages.elements.GUIDPrefix;
 import org.fiware.kiara.ps.rtps.participant.RTPSParticipant;
@@ -8,9 +11,35 @@ import org.fiware.kiara.ps.rtps.resources.ListenResource;
 import org.fiware.kiara.ps.rtps.utils.IPFinder;
 
 public class NewMain {
-	
+	        static void displayInterfaceInformation(java.net.NetworkInterface netint) throws SocketException {
+            System.out.printf("Display name: %s\n", netint.getDisplayName());
+            System.out.printf("Name: %s\n", netint.getName());
+            java.util.Enumeration<java.net.InetAddress> inetAddresses = netint.getInetAddresses();
+            for (java.net.InetAddress inetAddress : java.util.Collections.list(inetAddresses)) {
+                System.out.printf("InetAddress: %s\n", inetAddress);
+                if (inetAddress.isLoopbackAddress()) {
+                    System.out.printf("Loopback address%n");
+                }
+                if (inetAddress instanceof java.net.Inet6Address) {
+                    System.out.printf("IPv6 address%n");
+                }
+                if (inetAddress instanceof java.net.Inet4Address) {
+                    System.out.printf("IPv4 address%n");
+                }
+                System.out.printf("%n");
+            }
+            System.out.printf("%n");
+        }
+
 	public static void main(String[] args) {
-		
+                try {
+                    java.util.Enumeration<java.net.NetworkInterface> nets = java.net.NetworkInterface.getNetworkInterfaces();
+                    for (java.net.NetworkInterface netint : java.util.Collections.list(nets))
+                        displayInterfaceInformation(netint);
+                } catch (SocketException ex) {
+                    Logger.getLogger(NewMain.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
 		//IPFinder.getIPs();
 		
 		ListenResource lr = new ListenResource(new RTPSParticipant(null, null, null), 1, true);
