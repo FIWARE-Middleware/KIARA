ed27c955a0dff7276b676443a3467089c2f3b446
Rafael Lara <RafaelLara@eprosima.com>
2015-06-19 11:46:55 +0200

    Added copyright header in all classes

--- src/main/java/org/fiware/kiara/ps/rtps/attributes/HistoryCacheAttributes.java
+++ src/main/java/org/fiware/kiara/ps/rtps/attributes/HistoryCacheAttributes.java
@@ -1,3 +1,20 @@
+/* KIARA - Middleware for efficient and QoS/Security-aware invocation of services and exchange of messages
+ *
+ * Copyright (C) 2015 Proyectos y Sistemas de Mantenimiento S.L. (eProsima)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.fiware.kiara.ps.rtps.attributes;
 
 public class HistoryCacheAttributes {
